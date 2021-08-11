4014320664726292ffd4bd7db3ef890b932514a1
Mihai Panaitoiu <varusovarus1328@gmail.com>
2021-08-11 14:55:49 +0300

    Adding files

---
+++ index.js
@@ -0,0 +1,64 @@
+const express = require('express');
+const app = express();
+const path = require('path');
+const mongoose = require('mongoose');
+const methodOverride = require('method-override');
+
+const Product = require('./models/product');
+
+mongoose.connect('mongodb://localhost:27017/farmStand', { useNewUrlParser: true, useUnifiedTopology: true })
+    .then(() => {
+        console.log('MONGO connection OPEN!')
+    })
+    .catch(err => {
+        console.log("oh no !MONGO connection error")
+        console.log(err);
+    })
+
+
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+app.use(express.urlencoded({ extended: true }))
+app.use(methodOverride('_method'));
+
+const categories = ['fruit', 'vegetable', 'dairy', 'fungi']
+
+
+app.get('/products/new', (req, res) => {
+    res.render('products/new', { categories })
+})
+
+app.post('/products', async (req, res) => {
+    const newProduct = new Product(req.body)
+    await newProduct.save();
+    res.redirect(`/products/${newProduct._id}`)
+})
+
+app.get('/products', async (req, res) => {
+    //finding all products
+    const products = await Product.find({})
+    res.render('products/index', { products })
+})
+
+app.get('/products/:id', async (req, res) => {
+    const { id } = req.params;
+    const product = await Product.findById(id);
+    console.log(product);
+    res.render('products/show', { product })
+})
+
+app.get('/products/:id/edit', async (req, res) => {
+    const { id } = req.params
+    const product = await Product.findById(id);
+    res.render('products/edit', { product, categories })
+})
+app.put('/products/:id', async (req, res) => {
+    const { id } = req.params;
+    const product = await Product.findByIdAndUpdate(id, req.body, { runValidators: true, new: true })
+    console.log(req.body)
+    res.redirect(`/products/${product._id}`);
+})
+
+app.listen(3000, () => {
+    console.log('app is listening to port 3000')
+})
---
+++ models/product.js
@@ -0,0 +1,24 @@
+const mongoose = require('mongoose');
+
+const productSchema = new mongoose.Schema(
+    {
+        name: {
+            type: String,
+            required: true
+        },
+        price: {
+            type: Number,
+            required: true,
+            min: 0
+        },
+        category: {
+            type: String,
+            enum: ['fruit', 'vegetable', 'dairy', 'fungi'],
+            lowercase: true
+        }
+    }
+)
+
+const Product = mongoose.model('Product', productSchema);
+
+module.exports = Product;
